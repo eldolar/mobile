3492939af2609112a6a45a581781efbfd7ee3775
ezedepetris <ezequiel.depetris@utilitynyc.com>
2019-08-11 21:28:46 -0300

    Add description to about

--- src/screens/About/index.js
+++ src/screens/About/index.js
@@ -3,16 +3,16 @@ import { View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from "react-native-vector-icons/Ionicons";
 import { HeaderGoBack } from "../../components/Header";
+import AppDescription from "../../components/AppDescription";
 import { FooterAbout } from "../../components/Footer";
 import Layout from "../../layout";
-import styles from './styles';
 
 class About extends Component {
   render() {
     return (
       <Layout>
         <HeaderGoBack navigation={this.props.navigation}/>
-        <View style={{flex: 1}}></View>
+        <AppDescription/>
         <FooterAbout navigation={this.props.navigation}/>
       </Layout>
     )
