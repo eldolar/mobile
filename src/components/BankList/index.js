1f8fd766a95d2b4cc20e6efa5b8280e31f774574
ezedepetris <ezequiel.depetris@utilitynyc.com>
2019-08-11 21:30:25 -0300

    Add refrest to back list component

--- src/components/BankList/index.js
+++ src/components/BankList/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, RefreshControl } from 'react-native';
 import Bank from "../Bank";
+import PropTypes from 'prop-types';
 
-const BankList = ({banks}) => (
-  <ScrollView>
-    { banks.map(bank => <Bank name={bank.name} sell={bank.sell} buy={bank.buy}/> )}
+const BankList = ({banks, refresh, loading}) => (
+  <ScrollView
+    showsVerticalScrollIndicator={false}
+    refreshControl={
+      <RefreshControl
+        refreshing={loading}
+        onRefresh={refresh}
+      />
+    }>
+    { banks.map((bank, index) => <Bank key={index} name={bank.name} sell={bank.sell} buy={bank.buy}/> )}
   </ScrollView>
 )
 
-
 export default BankList
 
+BankList.defaultProps = {
+  loading: false,
+  refresh: () => {},
+  banks: [],
+};
+
+BankList.propTypes = {
+  loading: PropTypes.bool,
+  refresh: PropTypes.func,
+  banks: PropTypes.array
+};
